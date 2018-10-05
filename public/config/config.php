8702f3b1e91208f492b455bfa84336f1d3bebacb
Patrick Schumacher <hello@thepoddi.com>
2018-10-06 00:55:50 +0200

    move secret keys to general wp-config

--- public/config/config.php
+++ public/config/config.php
@@ -9,14 +9,3 @@ define( 'DB_HOST', '' );
 ini_set( 'display_errors', E_ALL );
 define( 'WP_DEBUG_DISPLAY', true );
 define( 'WP_DEBUG', true );
-
-// secret keys
-// @link https://api.wordpress.org/secret-key/1.1/salt/
-define('AUTH_KEY',         '');
-define('SECURE_AUTH_KEY',  '');
-define('LOGGED_IN_KEY',    '');
-define('NONCE_KEY',        '');
-define('AUTH_SALT',        '');
-define('SECURE_AUTH_SALT', '');
-define('LOGGED_IN_SALT',   '');
-define('NONCE_SALT',       '');
--- public/wp-config.php
+++ public/wp-config.php
@@ -24,6 +24,17 @@ ini_set( 'display_errors', 0 );
 define( 'WP_DEBUG_DISPLAY', false );
 define( 'WP_DEBUG', false );
 
+// secret keys
+// @link https://api.wordpress.org/secret-key/1.1/salt/
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
+
 // automatic updates
 define( 'AUTOMATIC_UPDATER_DISABLED', false );
 
